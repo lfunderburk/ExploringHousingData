678e70944ca1b75fb6d09c8f91dca148db0f6828
Laura Gutierrez Funderburk <laura_gutierrez_funderburk@sfu.ca>
2021-05-03 18:01:33 -0700

    adding extra data sources
    adding requirements and main program

---
+++ scr/main.py
@@ -0,0 +1,39 @@
+from stats_can import StatsCan
+import pandas as pd
+
+def get_stats_can_data(table: str):
+    """
+
+    :param sc: statscan instance
+    :param table: (str) contains the table number associated with
+                    table in Statistics Canada
+    :return: stats_df: (dataframe object) dataframe returned after
+                    downloading data
+    """
+    # Initialize StatsCan instance
+    sc = StatsCan()
+    stats_df = sc.table_to_df(table)
+    print(sc.downloaded_tables)
+
+    return stats_df
+
+
+def main():
+    """
+    Main program
+    :return:
+    """
+    # Sample getting data with StatsCan -
+    # https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=1810020502
+    table = "18-10-0205-02"
+    housing_price_index = get_stats_can_data(table)
+    print(housing_price_index.head())
+
+    # Sample reading data from downloaded file
+    relative_path_sm_res = "./ExploringHousingData/data/46100038.csv"
+    single_multiple_residence_df = pd.read_csv(relative_path_sm_res)
+    print(single_multiple_residence_df.head())
+
+
+if __name__ == '__main__':
+    main()
