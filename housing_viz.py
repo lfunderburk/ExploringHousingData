8aa1a1b1fc8892ba3b5a581232f9741bfa8ab560
Hanh Tong <hanh.tong@tap.work>
2021-06-17 18:03:47 -0700

    Modify dashboard

--- housing_viz.py
+++ housing_viz.py
@@ -6,70 +6,27 @@ import plotly.express as px
 from jupyter_dash import JupyterDash
 import pandas as pd
 
+import warnings
+from pandas.core.common import SettingWithCopyWarning
+warnings.simplefilter(action="ignore", category=SettingWithCopyWarning)
 
-# ----------------------------- functions for data preparation ------------------------------------------
-def data_prep(data_file):
-    #Reading the data and making a copy
-    df = pd.read_excel(data_file)
-    temp = df.copy()
-
-    #Removing empty rows
-    temp.dropna(thresh=2, inplace=True)
-
-    #Reseting columns - Changing the column names using the values in the 1st row
-    temp_header = temp.iloc[0,]
-    temp = temp[1:]
-    temp.columns = temp_header
-
-    #Removing all columns whose names are nan
-    temp = temp.loc[:, temp.columns.notnull()]
-
-    #Ensuring the indices go from 0 without skipping any number
-    temp.reset_index(inplace=True, drop = True)
-
-    #Categorize Geography as Country, Province, and City
-    temp['Region/City'] = ""
-    temp['Region/City'][temp.index ==0] = 'Country'
-    temp['Region/City'][(temp.index < 11) & (temp.index >0)] = 'Province'
-    temp['Region/City'][temp.index >= 11] = 'City'
-
-    #Rearrange columns -- bring column 'Region/City' to the 1st column position
-    # Access columns
-    cols = temp.columns.tolist()
-    # Bring last col to front
-    cols = cols[-1:] + cols[:-1]
-    # Set new column order
-    temp = temp[cols] 
-
-    #Converting data type to the right format
-    #Isolating yearly and quarterly columns and convert to numeric.
-    int_cols = temp.columns.drop(['Region/City', 'Geography'])
-    temp[int_cols] = temp[int_cols].apply(pd.to_numeric, errors='coerce')
-
-    return temp
-
-
-def slice_data(df, level):
-    """
-    df: data frame with mortgage data
-    level: "Province" or "City"
-    Extract a subset of df based on level
-    Return a dataframe
-    """
-    try:
-        temp = df[df['Region/City']==level]
-        temp = pd.melt(temp, id_vars='Geography', value_vars=temp.columns[2:])
-        temp.rename(columns = {3:'Time'}, inplace = True)
-        return temp
-    except KeyError:
-        print("Key not found. Make sure that 'level' is in ['Province','City']")
+import data_cleaning  
 
 #------------------------------------ read in the data ---------------------------------------------------
 data = "./data/"
-data_file = data + "average-value-new-mortgage-loans-ca-prov-cmas-2012-q3-2020-q3-en.xlsx"
-df_mortgage = data_prep(data_file)
 level = 'Province'
-df_mortgage_long = slice_data(df_mortgage, level = level)
+# mortgage data
+data_file = data + "average-value-new-mortgage-loans-ca-prov-cmas-2012-q3-2020-q3-en.xlsx"
+df_mortgage = data_cleaning.data_prep(data_file)
+df_mortgage_long = data_cleaning.slice_data(df_mortgage, level = level)
+
+# income data
+data_income = data + "real-average-household-income-after-taxes-tenure-2006-2018-en.xlsx"
+df_income = data_cleaning.data_prep(data_income)
+df_income_long = data_cleaning.slice_data(df_income, level = level)
+
+# combine 2 data set
+df_mortgage_income = data_cleaning.combine_data(df_mortgage, df_income)
 
 #-------------------------------------- APP section ------------------------------------------------------
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -77,37 +34,54 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = JupyterDash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div([
-    dcc.Graph(id='graph-with-dropdown'),
     dcc.Dropdown(
         id='province',
         options=[{'label': i, 'value': i} for i in df_mortgage_long['Geography'].unique()],
         value= 'Newfoundland'
-    )
+    ),
+    
+    dcc.Graph(id='graph-mortgage'),
+    dcc.Graph(id='graph-mortgage-income')
+    
 ])
 
-#-----------------------------------------------------
 @app.callback(
-    Output('graph-with-dropdown', 'figure'),
+    Output('graph-mortgage', 'figure'),
     Input('province', 'value'))
 def update_figure(selected_province):
-    filtered_df = df_mortgage_long[df_mortgage_long['Geography'] == selected_province]
+    df = df_mortgage_long
+    filtered_df = df[df['Geography'] == selected_province]
 
-    fig = px.line(filtered_df, x="Time", y="value", 
+    fig1 = px.line(filtered_df, x="Time", y="value", 
                   title = f'Line plot of mortgage loans in {selected_province}', 
                   hover_name="value")
-    fig.update_xaxes(tickangle=-45)
-    fig.update_layout(transition_duration=500)
+    fig1.update_xaxes(tickangle=-45)
+    fig1.update_layout(transition_duration=500)
 
-    return fig
+    return fig1
 
+
+@app.callback(
+    Output('graph-mortgage-income', 'figure'),
+    Input('province', 'value'))
+def mortgage_income(selected_province):
+    df = df_mortgage_income
+     
+    #df[df.Income.T.index == selected_province]
+    
+    fig2 = px.scatter(df.Income[selected_province]/df.Mortgage[selected_province]*100, 
+                      title = f'Time series of income/mortgage in {selected_province}',
+                     hover_name='value')
+    return fig2
 
 #-----------------------------------------------------------------------
-#fig = px.line(df_mortgage_long, x='Time', y='value', color = 'Geography',
-#              title = f'Line plot of mortgage loans by {level}'.upper())
-#fig.update_xaxes(tickangle=-45)
-#fig.show()
-
-#-----------------------------------------------------------------------
-if __name__ == '__main__':
+if __name__ == '__main__':  
     app.run_server(mode='inline')  # 'inline' so that we don't have to open a new browser
-   
+
+#   
+# fig = px.scatter(pivot_income_province.Income/ pivot_income_province.Mortgage*100, 
+#           facet_col="Geography",
+#          title="Income to mortgage by province")
+
+#fig.for_each_annotation(lambda a: a.update(text=a.text.split("=")[-1]))
+#fig.show()
